dfaeaf6691b18c73a96dc69c7e538deabd8d8886
ttufford <67388096+ttufford@users.noreply.github.com>
2020-07-23 06:40:25 -0700

    3

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
-
-//$collection = (new MongoDB\Client)->hddatabase->books;
-
 //db connection
 $manager = new MongoDB\Client(
   'mongodb+srv://<username>:<pass>@cluster0-s8mjc.azure.mongodb.net/mydb?retryWrites=true&w=majority');
