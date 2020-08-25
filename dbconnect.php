3bb594d1cf3e01a4cac6b0d7eede3f8f5dbcf72c
ttufford <67388096+ttufford@users.noreply.github.com>
2020-08-25 14:18:18 -0700

    updated output

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 //db connection
 $manager = new MongoDB\Client(
-  'mongodb+srv://ttuff:402Pass999@cluster0-s8mjc.azure.mongodb.net/mydb?retryWrites=true&w=majority');
+  'mongodb://phpsalt:L3IHhZLLvYiNQbOXDgAbGSsA1pTctCZtMW0bjNIcepeZ8kaSZkAS4dCqy467xCnxog15I2RHbFC2vrrdClBqWg==@phpsalt.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@phpsalt@');
 
 //select db
 $db = $manager->mydb;
